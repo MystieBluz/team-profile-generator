4f9e6693c6c285c81911e07fa39ddb904249634d
Misty Duhart <mystie.smith@gmail.com>
2022-05-04 21:01:36 -0400

    started to create questions for array and need to follow up and complete

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// Create questions -- figure out how to provide answers to correct chosen title
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the Manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the Manager?'
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the Manager?'
+    },
+];
