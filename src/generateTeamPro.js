dcd2a23d481ed76f1ca2decb3d89e8857904f068
Misty Duhart <mystie.smith@gmail.com>
2022-05-09 10:50:25 -0400

    added funtion to generate team cards

--- src/generateTeamPro.js
+++ src/generateTeamPro.js
@@ -51,13 +51,92 @@ const createIntern = (intern) => {
 </div>`
 }
 
-// Function to generate Team Profile
+// push array to page 
 generateTeamPro = (data) => {
-    teamCards = [];
-    for (let i = 0; i < data.length; i++){
+
+    // array for cards 
+    pageArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
         const employee = data[i];
-        const title = employee.teamTitle
+        const role = employee.createTitle(); 
+
+        if (role === 'Manager') {
+            const managerCard = createManager(employee);
+
+            pageArray.push(managerCard);
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = createEngineer(employee);
+
+            pageArray.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = createIntern(employee);
+
+            pageArray.push(internCard);
+        }
+        
     }
+
+    // joining strings 
+    const teamCards = pageArray.join('')
+
+    // return to generated page
+    const createTeam = generateTeamPage(teamCards); 
+    return createTeam;
+
+}
+
+const generateTeamPage = function (teamCards){
+    retun `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+    
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" integrity="sha384-ejwKkLla8gPP8t2u0eQyL0Q/4ItcnyveF505U0NIobD/SMsNyXrLti6CWaD0L52l" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    
+    <body>
+        <!-- My Team Header -->
+        <header>
+            <div class="jumbotron jumbotron-fluid p-3 mb-2 bg-transparent text-grey text-center">
+                <div class="container">
+                  <h1 class="display-4">My Team</h1>
+                  <figure class="text-center">
+                    <blockquote class="blockquote">
+                      <p>"Teamwork Makes The Dream Work."</p>
+                    </blockquote>
+                    <figcaption class="blockquote-footer">
+                       <cite title="Source Title">John C. Maxwell</cite>
+                    </figcaption>
+                  </figure>
+                </div>
+              </div>
+    
+        </header>
+              <main>
+                  <div class="continaer">
+                      <div class="row justify-content-center">
+                        ${employeeCards}
+                      </div>
+                  </div>
+              </main>
+     
+    
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    
+    </body>
+    </html>`
 }
 
 
